99cb80bbcdb571b33fabdbf2afcf9c7ee58cc3d0
Millena <millenamariaoficial@gmail.com>
2023-09-12 12:03:33 -0300

    Trabalhando com Objetos II

---
+++ exercicio1/scrip.js
@@ -0,0 +1,88 @@
+const carros = [
+    {
+        modelo: "Ka",
+        marca: "Ford",
+        ano: "2000",
+        cor: "Branco",
+        completo: false,
+        acessorios: ['Vidro Elétrico'],
+        preco: 6799.33
+    },
+    {
+        modelo: "Gol",
+        marca: "VW",
+        ano: "1996",
+        cor: "Preto",
+        completo: false,
+        acessorios: ['Trava'],
+        preco: 12199.13
+    },
+    {
+        modelo: "Palio",
+        marca: "Fiat",
+        ano: "1995",
+        cor: "Verde",
+        completo: false,
+        acessorios: [],
+        preco: 11099.1
+    },
+    {
+        modelo: "Monza",
+        marca: "Chevrolet",
+        ano: "1993",
+        cor: "Vinho",
+        completo: false,
+        acessorios: [],
+        preco: 14578.25
+    },
+    {
+        modelo: "Saveiro",
+        marca: "VW",
+        ano: "2013",
+        cor: "Prata",
+        completo: false,
+        acessorios: [],
+        preco: 28399.13
+    },
+    {
+        modelo: "Gol",
+        marca: "VW",
+        ano: "1996",
+        cor: "Preto",
+        completo: true,
+        acessorios: ['Alarme', 'Trava', 'Ar', 'Vidro Elétrico'],
+        preco: 14350.45
+    },
+    {
+        modelo: "Gol",
+        marca: "VW",
+        ano: "2013",
+        cor: "Preto",
+        completo: true,
+        acessorios: ['Alarme', 'Trava', 'Ar', 'Vidro Elétrico'],
+        preco: 22109.21
+    },
+    {
+        modelo: "Montana",
+        marca: "Chevrolet",
+        ano: "2011",
+        cor: "Azul",
+        completo: false,
+        acessorios: [],
+        preco: 15999.13
+    },
+    {
+        modelo: "Stilo",
+        marca: "Fiat",
+        ano: "2000",
+        cor: "Preto",
+        completo: false,
+        acessorios: [],
+        preco: 17251.36
+    }
+]
+
+function contaTotal() {
+    return `Quantidade total de veículos: ${carros.length}`;
+}
+console.log(contaTotal());
